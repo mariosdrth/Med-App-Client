ee6d68f7160309279dc48a9094a8c1eb0d83a32d
Marios Papamarkou <mariosdrth@yahoo.gr>
2019-08-24 12:34:01 +0200

    consts

--- src/app/services/global-parameters/global-parameters.service.ts
+++ src/app/services/global-parameters/global-parameters.service.ts
@@ -10,7 +10,7 @@ export enum Types {
 }
 
 export enum Constants {
-  //generalUrl = "http://localhost:8080",
+  //generalUrl = "http://localhost:8089",
   //generalUrl = "http://192.168.1.4:8080",
   generalUrl = "http://samplesite.ddns.net:8089",
   emailReceiverAddress = "mariosdrth@yahoo.gr"
