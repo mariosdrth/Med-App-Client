1b2af2cb368b35a38878650b4da1534f926250a2
Marios Papamarkou <mariosdrth@yahoo.gr>
2019-09-21 21:16:25 +0200

    Settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import * as envars from '../assets/envars.json';
+
 export const environment = {
   production: false,
   APIEndpoint: 'http://samplesite.ddns.net:8089'
+  //APIEndpoint: envars.default.apiUrlDev
 };
 
 /*
